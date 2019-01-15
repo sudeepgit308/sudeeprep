f5621511002bde78426d41fa408f6f2d5bb2143e
sudeep <sudeep@308>
2019-01-15 19:11:59 +0530

    added some logic

--- logicalprograms/src/com/nt/test/ArrayElementRemovalDemo.java
+++ logicalprograms/src/com/nt/test/ArrayElementRemovalDemo.java
@@ -3,6 +3,7 @@ package com.nt.test;
 public class ArrayElementRemovalDemo {
 
 	public static void main(String[] args) {
+		String[] arrayBeforeRemoval=new String[] {"sudeep","bunny","neha","rasool"};
  
 	}
 
